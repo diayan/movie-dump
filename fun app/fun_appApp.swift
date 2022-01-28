c5b2554f578973e04652ca58c04393e8348ada5d
diayan <diayans@outlook.com>
2022-01-28 18:36:09 +0000

    Initial Commit

---
+++ fun app/fun_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fun_appApp.swift
+//  fun app
+//
+//  Created by diayan siat on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct fun_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
